773ee6b9222a70308b6d1b3c9a93eb58db2227d7
Jesus Martin Blanco <jesusmartinb64@gmail.com>
2023-06-10 21:02:47 +0200

    se implementa la subida al servidor de la foto de perfil

--- models/personal.model.js
+++ models/personal.model.js
@@ -1,16 +1,23 @@
-const create = ({nombre, apellidos, fecha_nacimiento, foto, direccion, ciudad, codigo_postal, longitud, latitud,telefono, usuario_id}) => {
+const create = ({ nombre, apellidos, fecha_nacimiento, foto, direccion, ciudad, codigo_postal, longitud, latitud, telefono, usuario_id }) => {
     return db.query(
         'insert into datos_personales (nombre, apellidos, fecha_nacimiento, foto, direccion, ciudad, codigo_postal, longitud, latitud,telefono,  usuario_id) values (?,?,?,?,?,?,?,?,?,?,?)',
-        [nombre, apellidos, fecha_nacimiento, foto, direccion, ciudad, codigo_postal, longitud, latitud,telefono,  usuario_id]
+        [nombre, apellidos, fecha_nacimiento, foto, direccion, ciudad, codigo_postal, longitud, latitud, telefono, usuario_id]
     )
 
 }
 
-const getById  = (insertId) => {
-    return db.query('select * from datos_personales where  id =?',[insertId]);
+const getById = (insertId) => {
+    return db.query('select * from datos_personales where  id =?', [insertId]);
 
 }
 
+const upload = (id, { foto }) => {
+    return db.query(
+        'update datos_personales set foto = ? where usuario_id = ?',
+        [foto, id]
+    )
+}
+
 module.exports = {
-    create, getById
+    create, getById, upload
 }
